7649847a14f307a5af61cf5fae34de79ff0cc28b
Peter Ahlers <p.ahlers@gmail.com>
2019-12-01 20:14:52 +0100

    feat(redox101): Both parts has BSPC and DEL keys

--- keyboards/redox101/keymaps/default/keymap.c
+++ keyboards/redox101/keymaps/default/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,KC_1,   KC_2,  KC_3,    KC_4,  KC_5,    KC_GRV,                      KC_EQL,  KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_F5,   \
     KC_F1, KC_Q,   KC_W,  KC_E,    KC_R,  KC_T,    KC_QUOT,                     KC_MINS, KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_F6,   \
     KC_F2, KC_A,   KC_S,  KC_D,    KC_F,  KC_G,    KC_LBRC,                     KC_RBRC, KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, KC_F7,   \
-    KC_F3, KC_Z,   KC_X,  KC_C,    KC_V,  KC_B,    KC_BSPC,                     KC_DEL,  KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_UP,   KC_F8,   \
+    KC_F3, KC_Z,   KC_X,  KC_C,    KC_V,  KC_B,    KC_BSPC,                     KC_BSPC,  KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_UP,   KC_F8,   \
     KC_F4, KC_TAB, TT(2), KC_LGUI, MO(1), KC_LCTL, KC_COLN,                     KC_SCLN, KC_SPC, KC_RSFT, KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT, \
                                           KC_LALT,                                       KC_ENT                                               \
   ),
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LT,                         KC_GT,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F13, \
     KC_F9,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                       KC_PIPE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F14, \
     KC_F10,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LPRN,                       KC_RPRN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F15, \
-    KC_F11,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_F16, \
+    KC_F11,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL,                        KC_DEL, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_F16, \
     KC_F12,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BSLS,                       KC_PSLS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_END, \
                                                  KC_TRNS,                                         KC_TRNS                                              \
   ),
